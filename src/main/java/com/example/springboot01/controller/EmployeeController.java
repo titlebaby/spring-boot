2438b8d1ec05e9fdad12f1b0c60c9abdd5dd3aff
lyl <272592782@qq.com>
2022-07-15 12:41:51 +0800

    thymeleaf的简单增删查改demo

--- src/main/java/com/example/springboot01/controller/EmployeeController.java
+++ src/main/java/com/example/springboot01/controller/EmployeeController.java
@@ -7,10 +7,7 @@ import com.example.springboot01.pojo.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -20,30 +17,43 @@ public class EmployeeController {
     EmployeeDao employeeDao;
     @Autowired
     DepartmentDao departmentDao;
+
     @RequestMapping("/emps")
-    public String list(Model model){
+    public String list(Model model) {
         Collection<Employee> employees = employeeDao.getAll();
         model.addAttribute("emps", employees);
         return "emp/list.html";
 
     }
+
     @GetMapping("/emp")
-    public String toAddPage(Model model){
+    public String toAddPage(Model model) {
         Collection<Department> departments = departmentDao.getDepartment();
-        model.addAttribute("departments",departments);
+        model.addAttribute("departments", departments);
         return "emp/add.html";
     }
 
     @PostMapping("/emp")
-    public String addEmp(Employee employee){
+    public String addEmp(Employee employee) {
         //添加的操作
         System.out.println(employee);
         employeeDao.save(employee);
         return "redirect:/emps";
     }
-    @GetMapping("/upadte")
-    public String toUpdateEmp(){
+
+    @GetMapping("/emp/{id}")
+    public String toUpdateEmp(@PathVariable("id") Integer id, Model model) {
+        System.out.println(id);
+        Employee employee = employeeDao.getEmployeeById(id);
+        model.addAttribute("emp",employee);
+        Collection<Department> departments = departmentDao.getDepartment();
+        model.addAttribute("departments", departments);
         return "emp/update";
+    }
+    @PostMapping("/updateEmp")
+    public String updateEmp(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/emps";
     }
 }
 
