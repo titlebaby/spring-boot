e4296966ca53714ad3f01ab92bf69615b029b84e
lyl <272592782@qq.com>
2022-07-19 11:54:34 +0800

    spring security的登陆验证1

--- src/main/java/com/example/springboot01/config/SecurityConfig.java
+++ src/main/java/com/example/springboot01/config/SecurityConfig.java
@@ -14,16 +14,18 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
        // super.configure(http);
         //首页所有人可以访问，功能也只有对应权限的人才能访问
         http.authorizeRequests()
-                .antMatchers("/auth_index")
-                .permitAll()
+                .antMatchers("/auth_index").permitAll()
                 .antMatchers("/level1/**").hasRole("vip1")
                 .antMatchers("/level2/**").hasRole("vip2")
                 .antMatchers("/level3/**").hasRole("vip3");
         // aop 切面编程
         // 没有权限默认要到登陆页
-        http.formLogin().loginPage("/toLogin")
+        http.formLogin()
+                // 指定登陆界面，没有就是默认的登陆界面
+                .loginPage("/toLogin")
                 .usernameParameter("username")
                 .passwordParameter("password")
+                // 指定验证用户名和密码的地址
                 .loginProcessingUrl("/login");
 
 
